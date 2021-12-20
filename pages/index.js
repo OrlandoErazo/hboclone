f7c9c2e195d561f9bfab2901047f5807d429d9f2
OrlandoErazo <o_erazo@hotmail.com>
2021-12-20 15:00:53 -0500

    login css styles

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>
-      test
-    </h1>
-  )
+    <div>
+      <div className="login-user">
+        <div className="login-user__top">
+          <div className="login-user__logo"></div>
+          <span className="login-user__title">
+            Who is Watching?
+          </span>
+        </div>
+
+<div className="login-user__form">
+  <div className="login-user__user-box">
+    <img className="login-user__user-img" src="https://uifaces.co/our-content/donated/vIqzOHXj.jpg"></img>
+    <div className="login-user__user-name">Bryant</div>
+  </div>
+</div>
+<div className="login-user__buttons">
+  <button className="login-user__adult">Add Adult</button>
+  <button className="login-user__kid">Add Kid</button>
+</div>
+      </div>
+    </div>
+  );
 }
