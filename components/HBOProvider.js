8da4a2796a54137a3e53ff3cf367971583d2dc85
OrlandoErazo <o_erazo@hotmail.com>
2022-02-07 00:31:14 -0500

    Save User to Local Storage

--- components/HBOProvider.js
+++ components/HBOProvider.js
@@ -7,7 +7,7 @@ export function useStateContext(){
 }
 
 export function HBOProvider({children}){
-const [user, setUser] = useState('')
+const [user, setUser] = useState()
 const defaultUserImg = 'https://images.generated.photos/0nDYZvW2PyRknrz8QtVclfVvXVllxOnNx1t-BGnNve0/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzI1Mzc0LmpwZw.jpg'
 const CreateUserAction = (e) => {
   setUser(e.target.value)
--- pages/create.js
+++ pages/create.js
@@ -1,9 +1,43 @@
 import Head from "next/head";
-import { useStateContext } from '../components/HBOProvider'
+import { useStateContext } from '../components/HBOProvider';
+import ls from 'local-storage';
+import {v4} from 'uuid';
+import { useRouter } from "next/router";
+
 
 export default function CreateUser() {
   const globalState = useStateContext();
-  console.log(globalState)
+  const router = useRouter();
+  const saveUser = () => {
+    let users = [],
+    user;
+
+    if(ls('users') < 1){
+      
+      user ={
+        id: v4(),
+        user: globalState.user,
+        myListID: [] 
+      }
+      users.push(user)
+      ls('users', users)
+
+      console.log('users:', users)
+      console.log('lsusers', ls('users'))
+    } else {
+      users = ls('users')
+      user ={
+        id: v4(),
+        user: globalState.user,
+        myListID: [] 
+      }
+      users.push(user)
+      ls('users', users)
+      console.log('users:', users)
+      console.log('lsusers', ls('users'))
+      router.push('/Login')
+    }
+  }
   return (
     <div>
       <div className="create-user">
@@ -19,7 +53,7 @@ export default function CreateUser() {
           ></img>
           <div className="create-user__input-group">
             <label>Name</label>
-            <input value={globalState.user} onChange={globalState.createUserAction} type="text" className="create-user__inputText" />
+            <input value={ globalState.user } onChange={globalState.createUserAction} type="text" className="create-user__inputText" />
             <div className="create-user__colors">
               <div
                 className="create-user__color create-user__color--active"
@@ -66,7 +100,7 @@ export default function CreateUser() {
         </div>
       <div className="create-user__buttons">
         <button className="create-user__cancel">Cancel</button>
-        <button className="create-user__save">Save</button>
+        <button className="create-user__save" onClick= {saveUser}>Save</button>
       </div>
       </div>
     </div>
