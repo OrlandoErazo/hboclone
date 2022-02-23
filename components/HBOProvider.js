41e9476a2e488e780a08fc90ae5b50f8f6784216
OrlandoErazo <o_erazo@hotmail.com>
2022-02-23 17:38:15 -0500

    Open the SideNav and Account modal

--- components/HBOProvider.js
+++ components/HBOProvider.js
@@ -8,18 +8,25 @@ export function useStateContext(){
 }
 
 export function HBOProvider({children}){
-const [user, setUser] = useState()
+const [user, setUser] = useState('')
 const defaultUserImg = 'https://images.generated.photos/0nDYZvW2PyRknrz8QtVclfVvXVllxOnNx1t-BGnNve0/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzI1Mzc0LmpwZw.jpg'
 const CreateUserAction = (e) => {
   setUser(e.target.value)
   console.log(user)
 }
+const [sideNavOpen, setSideNavOpenAction] = useState(false)
+const [accountModalOpen, setAccountModalOpenAction] = useState(false)
+
   return(
     <StateContext.Provider
     value={{
       user,
       CreateUserAction,
-      defaultUserImg
+      defaultUserImg,
+      sideNavOpen,
+      setSideNavOpenAction,
+      accountModalOpen,
+      setAccountModalOpenAction,
     }}>
       {children}
     </StateContext.Provider>
--- components/UI/Header/Header.js
+++ components/UI/Header/Header.js
@@ -1,12 +1,14 @@
 import Account from "../Account/Account";
 import SearchModal from "../SearchModal/SearchModal";
-
+import {useStateContext} from '../../HBOProvider'
 
 
 const Header = (props) => {
-  return(<header className="top-header">
+  const globalState = useStateContext();
+
+  return(<header className={`top-header ${globalState.accountModalOpen || globalState.sideNavModalOpen ? 'top-header--menu-open' : ''}`}>
     <div className="top-header__left-side">
-      <div className="top-header__menu-btn">
+      <div className="top-header__menu-btn" onClick={() => globalState.setSideNavOpenAction(true)}>
         <i className="fas fa-bars"/>
       </div>
       <div className="top-header__search-btn">
@@ -14,7 +16,7 @@ const Header = (props) => {
       </div>
     </div>
     <div className="top-header__logo"></div>
-    <div className="top-header__account">
+    <div className="top-header__account" onClick={() => globalState.setAccountModalOpenAction(!globalState.accountModalOpen)}>
       <img src="https://images.generated.photos/0nDYZvW2PyRknrz8QtVclfVvXVllxOnNx1t-BGnNve0/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzI1Mzc0LmpwZw.jpg" className="top-header__user-img"/>
       <div ClasssName="top-header__user-name">Bryant</div>
     </div>
